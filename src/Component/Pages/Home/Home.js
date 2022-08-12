150891ad1407367726be9541775d3594ea44bb22
minhazpro3 <minhazpro1@gmail.com>
2022-08-13 01:04:01 +0600

    Ui update

--- src/Component/Pages/Home/Home.js
+++ src/Component/Pages/Home/Home.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import About from '../About/About';
+import React, { useState } from 'react';
 import Services from '../Services/Services';
 import HomeCounter from '../HomeCounter/HomeCounter'
 import Banner from '../../Banner/Banner';
@@ -8,13 +7,14 @@ import DoctorsHome from '../DoctorsHome/DoctorsHome';
 import Footer from '../Footer/Footer';
 import LocationMap from '../LocationMap/LocationMap';
 import Pakages from '../Pakages/Pakages';
-
 const Home = () => {
+   
     return (
         <div>
            
             <Banner/>
             <Services/>
+            
             <HomeCounter/>
             <Pakages/>
             <Testimonial/>
