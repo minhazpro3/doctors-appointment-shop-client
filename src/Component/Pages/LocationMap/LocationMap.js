8fccdea9b2b874d78ce90f211eb44b1a648a8951
minhazpro3 <minhazpro1@gmail.com>
2022-12-24 14:29:42 +0600

    update input ui

--- src/Component/Pages/LocationMap/LocationMap.js
+++ src/Component/Pages/LocationMap/LocationMap.js
@@ -122,9 +122,9 @@ const LocationMap = () => {
               />
               <br />
 
-              <div className="inline-block shadow  relative bg-white  appearance-none border rounded w-60 md:w-80 lg:w-96  py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-3">
+              <div className="inline-block shadow  relative bg-white  appearance-none border rounded w-60 md:w-80 lg:w-96    text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-3">
                 <select
-                  className="block appearance-none w-full bg-white   pr-8 rounded  leading-tight focus:outline-none focus:shadow-outline"
+                  className="block appearance-none w-full bg-white py-3 border-0  pr-8 rounded  leading-tight focus:outline-none focus:shadow-outline"
                   {...register("doctor")}
                   required
                 >
@@ -135,16 +135,6 @@ const LocationMap = () => {
                     </option>
                   ))}
                 </select>
-
-                <div className="pointer-events-none  absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg
-                    className="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
-                </div>
               </div>
               <br />
 
--- src/Component/Pages/MakeAppointment/MakeAppointment.js
+++ src/Component/Pages/MakeAppointment/MakeAppointment.js
@@ -68,29 +68,17 @@ const MakeAppointment = () => {
             />
             <br />
 
-            <div className="inline-block shadow  relative   appearance-none border rounded w-60 md:w-80 lg:w-96  py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-3">
+            <div className="inline-block shadow  relative   border rounded w-60 md:w-80 lg:w-96   text-gray-700  focus:outline-none focus:shadow-outline mt-3">
               <select
                 className="block appearance-none w-full bg-white   pr-8 rounded  leading-tight focus:outline-none focus:shadow-outline"
                 {...register("doctor")}
                 required
               >
-                <option selected disabled>
-                  {" "}
-                  --Select Doctor--
-                </option>
+                <option disabled> --Select Doctor--</option>
                 {doctor.map((doc, index) => (
                   <option key={index}>{doc?.name}</option>
                 ))}
               </select>
-              <div className="pointer-events-none  absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
             </div>
             <br />
 
